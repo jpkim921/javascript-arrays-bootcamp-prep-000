78b197ce82f07eb7cd8b0799c264e467b74f4b24
jpkim921 <jpkim921@gmail.com>
2017-11-21 02:09:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,3 +22,12 @@ function addElementToEndOfArray(array, element){
 function accessElementInArray(array, index){
   return array[index]
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1)
+}
