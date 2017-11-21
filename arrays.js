8a572ffda417ed721d953bdbb0e01889997bb966
jpkim921 <jpkim921@gmail.com>
2017-11-21 01:02:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,10 @@
 var chocolateBars = ["snickers", "hundred grand", "katkit", "skittles"]
+
+function addElementToBeginningOfArray(array, element){
+  var newArray = [element, ...array]
+  return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  return array.unshift(element)
+}
