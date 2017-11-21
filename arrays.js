4453aa3d4dc2f26aeb627da7dd35c94aa3d53f02
jpkim921 <jpkim921@gmail.com>
2017-11-21 01:49:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,16 +5,16 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
 
-// function destructivelyAddElementToBeginningOfArray(array, element){
-//   array.unshift(element)
-//   return array
-// }
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
 
-// function destructivelyAddElementToEndOfArray(array, element){
-//   array.push(element)
-//   return array
-// }
-//
-// function addElementToEndOfArray(array, element){
-//   return [...array, element]
-// }
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element){
+  return [...array, element]
+}
