493822d594ec890de1a5ffc156cbcfff2f90115a
jpkim921 <jpkim921@gmail.com>
2017-11-21 01:18:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,21 @@
 var chocolateBars = ["snickers", "hundred grand", "katkit", "skittles"]
 
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  return array.unshift(element)
+}
+
 function addElementToBeginningOfArray(array, element){
   var newArray = [element, ...array]
   return newArray
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element){
+
+function destructivelyAddElementToEndOfArray(array, element){
   return array.unshift(element)
+}
+
+function addElementToEndOfArray(array, element){
+  var newArray = [element, ...array]
+  return newArray
 }
