67917f6a863d59c337a65c94ab7dbb5f1fbbd1d3
jpkim921 <jpkim921@gmail.com>
2017-11-21 01:23:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,17 +9,17 @@ function addElementToBeginningOfArray(array, element){
 // function destructivelyAddElementToBeginningOfArray(array, element){
 //   return array.unshift(element)
 // }
-// 
+//
 // function addElementToBeginningOfArray(array, element){
 //   var newArray = [element, ...array]
 //   return newArray
 // }
-// 
-// 
+//
+//
 // function destructivelyAddElementToEndOfArray(array, element){
 //   return array.push(element)
 // }
-// 
+//
 // function addElementToEndOfArray(array, element){
 //   var newArray = [...array, element]
 //   return newArray
